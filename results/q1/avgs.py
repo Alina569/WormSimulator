8c032780800dbe5240f20ccad1cdad4f890e91c6
Alina <alipercu@gmail.com>
2017-11-01 22:59:31 -0400

    Added statistics file

---
+++ results/q1/avgs.py
@@ -0,0 +1,35 @@
+
+total_num_machines = 65536
+def get_info(file_name):
+
+    data = [line.rstrip().split(" ") for line in open(file_name, "r")]
+
+    infection_times = []
+    attempts = []
+
+    total_time = 0
+    total_attempts = 0
+
+    for station_index, infect_time, attempt_num in data:
+        infection_times.append(float(infect_time))
+        attempts.append(int(attempt_num))
+
+        total_time += float(infect_time)
+        total_attempts += int(attempt_num)
+
+    avg_time = total_time / total_num_machines
+    avg_attempts = total_attempts // total_num_machines
+
+    min_attempt = min(attempts)
+    max_attempt = max(attempts)
+
+    return [avg_time, avg_attempts, min_attempt, max_attempt]
+
+# all the files
+files = ["infect_lcg.dat", "infect_phys.dat", "infect_unix_rand.dat", "infect_vn.dat"]
+
+for file_name in files:
+    print()
+    print(file_name)
+    print(get_info(file_name))
+    print()
