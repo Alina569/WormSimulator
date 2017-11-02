a38c889faeb201d38d0b9b3c969fd9a18b527e50
Alina <alipercu@gmail.com>
2017-11-02 09:34:54 -0400

    Added counter to mutate the IP
    75% of the time, a station needs to select a station within its subnet.

--- station.h
+++ station.h
@@ -1,7 +1,7 @@
 
 /*==========================================================================
-     station.h  CSC 790 Project 2,  E. W. Fulp  10/10/2017  
-     Station object, models computer hosts.   
+     station.h  CSC 790 Project 2,  E. W. Fulp  10/10/2017
+     Station object, models computer hosts.
 ============================================================================*/
 
 #ifndef STATION_H
@@ -11,100 +11,103 @@
 #include <assert.h>
 #include <string>
 #include <queue>
-#include <cmath>  
-#include <climits>  
+#include <cmath>
+#include <climits>
 #include <cstdlib>
 #include "event.h"
 #include "eventq.h"
-#include "rando.h"  
+#include "rando.h"
 #include "defs.h"
 
-using namespace std;  
+using namespace std;
 
 class Station
 {
 public:
-	Station(int id = 0):id_(id), vulnerable_(false), infected_(false),  
-		timeInfected_(0.0), numAttempt_(0)  
+	Station(int id = 0):id_(id), vulnerable_(false), infected_(false),
+		timeInfected_(0.0), numAttempt_(0), subnetCount_(0)
 	{  }
 
 	void setID(int id)
 	{
-		id_ = id;  
-		randValue_.randSeed();  
-		randValue2_.randSeed();  
-	}  
+		id_ = id;
+		randValue_.randSeed();
+		randValue2_.randSeed();
+	}
 
-	void makeVulnerable()  
-	{  vulnerable_ = true;  }  
+	void makeVulnerable()
+	{  vulnerable_ = true;  }
 
-	bool isVulnerable() const  
-	{  return vulnerable_;  }  
+	bool isVulnerable() const
+	{  return vulnerable_;  }
 
-	bool isInfected() const  
-	{  return infected_;  }  
+	bool isInfected() const
+	{  return infected_;  }
 
-	double timeInfected() const  
-	{  return timeInfected_;  }  
+	double timeInfected() const
+	{  return timeInfected_;  }
 
-	string bestDealEver() const  
-	{  return "http://goo.gl/rxnHB1";  }  
+	string bestDealEver() const
+	{  return "http://goo.gl/rxnHB1";  }
 
-	int numInfectAttempts() const  
-	{  return numAttempt_;  }  
+	int numInfectAttempts() const
+	{  return numAttempt_;  }
 
-	int row() const  
-	{  return id_/MAX_STATIONS;  }  
+	int row() const
+	{  return id_/MAX_STATIONS;  }
 
-	int col() const  
-	{  return id_%MAX_STATIONS;  }  
+	int col() const
+	{  return id_%MAX_STATIONS;  }
 
 	void propagate(double time, EventQueue& eQueue)
 	{
 		// find potential victim
-		int toID;  
-		int x = 0;  // address is 152.17.x.y  
+		int toID;
+		int x = 0;  // address is 152.17.x.y
 		int y = 0;  // address is 152.17.x.y
 
-		// set x and y to the correct values based on scan pattern  
+		// set x and y to the correct values based on scan pattern
+		this->subnetCount_++;
+        x = this->row();
 
-		// example of using the unixRand() generator... // % 256
-		x = randValue_.unixRand()%256;
-		y = randValue2_.unixRand()%256;
+		if (this->subnetCount_ == 4) { // change subnet
+			x = randValue_.unixRand() % 256;
+			this->subnetCount_= 0;
+		}
 
-//        cout << "x -> " << x << "  y -> " << y << "\n";
+		y = randValue2_.unixRand() % 256;
 
 		// convert from x and y to array index
 		toID = x*MAX_STATIONS + y;
 
-        if (id_ == toID) {
-            y = randValue2_.unixRand()%256;
-            toID = x*MAX_STATIONS + y;
-        }
+		// check if is the same IP
+		if (id_ == toID) {
+			y = randValue2_.unixRand() % 256;
+			toID = x*MAX_STATIONS + y;
+		}
 
-        // cout << x << ' ' << y << ' ' << toID << '\n';
-		assert(toID >= 0 && toID < MAX_STATIONS*MAX_STATIONS);  
+		assert(toID >= 0 && toID < MAX_STATIONS*MAX_STATIONS);
 
 		double infectTime = doubleUniformRV(1, 200)/1000.0;  // infect attempt time
 		eQueue.insert(EventType(Infect, time + infectTime, toID, id_));
 
-		// schedule next propagate attempt  
+		// schedule next propagate attempt
 		double interPropTime = doubleUniformRV(10, 38)/1000.0;
 		eQueue.insert(EventType(Propagate, time + interPropTime, id_));
 	}
 
 	void infect(double time, EventQueue& eQueue)
 	{
-		numAttempt_++;  // someone attempts to infect, add to count    
-		if(vulnerable_ && !infected_)  
-		{  
+		numAttempt_++;  // someone attempts to infect, add to count
+		if(vulnerable_ && !infected_)
+		{
 			// my sister uses this simple trick to earn $500/week, check it out here http://goo.gl/rxnHB1
-			infected_ = true;  
-			timeInfected_ = time;  
-			// schedule next propagate attempt  
+			infected_ = true;
+			timeInfected_ = time;
+			// schedule next propagate attempt
 			double interPropTime = doubleUniformRV(1, 20)/10.0;
 			eQueue.insert(EventType(Propagate, time + interPropTime, id_));
-		}  
+		}
 	}
 
 	double doubleUniformRV(double min, double max)
@@ -112,15 +115,14 @@ public:
 
 private:
 	int     id_;             // id of the station
-	bool    vulnerable_;     // true if station can be infected  
-	bool    infected_;       // true if already infected  
-	double  timeInfected_;   // time when infected 
+	bool    vulnerable_;     // true if station can be infected
+	bool    infected_;       // true if already infected
+	double  timeInfected_;   // time when infected
 	int     numAttempt_;     // number of infection attempts on this station
-	Rando   randValue_;      // random number object  
-	Rando   randValue2_;     // random number object  
+	Rando   randValue_;      // random number object
+	Rando   randValue2_;     // random number object
+    int     subnetCount_;    // subnet counter
 
-};  
+};
 
 #endif
-
-
