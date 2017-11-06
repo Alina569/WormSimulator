9b722ccda4b28ccad24b34336d70d7e09bd9e5b4
Alina <alipercu@gmail.com>
2017-11-05 21:16:05 -0500

    Updated plot.py for question 5. Added plot_patch

--- results/plot.py
+++ results/plot.py
@@ -16,7 +16,7 @@ def get_data(file_name):
     return times, infected_machines
 
 # all the files
-files = ["q1/data_lcg.dat", "q1/data_phys.dat", "q1/data_unix_rand.dat", "q1/data_vn.dat", "q2/data.dat", "q3/data.dat"]
+files = ["q5/patch_lcg.dat", "q5/patch_phys.dat", "q5/patch_unix.dat", "q5/patch_vn.dat", "q5/patch_topo.dat"]
 
 # data
 times1, infected1 = get_data(files[0])
@@ -24,17 +24,17 @@ times2, infected2 = get_data(files[1])
 times3, infected3 = get_data(files[2])
 times4, infected4 = get_data(files[3])
 times5, infected5 = get_data(files[4])
-times6, infected6 = get_data(files[5])
+#times6, infected6 = get_data(files[5])
 
 # plot the results
 plt.plot(times1, infected1, 'r--', label="LCG")
 plt.plot(times2, infected2, 'g:', label="Physical")
 plt.plot(times3, infected3, 'b-.', label="Unix")
 plt.plot(times4, infected4, 'y-', label="Von Neumann")
-plt.plot(times5, infected5, 'r:', label="Topological")
-plt.plot(times6, infected6, 'r-.', label="Hit List")
+plt.plot(times5, infected5, 'b-', label="Topological")
+#plt.plot(times6, infected6, 'r-.', label="Hit List")
 
-plt.ylabel("Infected Machines")
+plt.ylabel("Machines")
 plt.xlabel("Time")
 plt.legend()
 plt.show()
